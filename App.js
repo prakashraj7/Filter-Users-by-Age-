57752d209881bdf4801023dfad9e48aa3e7514db
prakashraj7 <prakashraj0497@gmail.com>
2022-07-21 14:59:39 +0530

    1 file modified

--- App.js
+++ App.js
@@ -3,20 +3,21 @@ import './style.css';
 
 export default function App() {
   const user = [
-    { firstName: 'Prakash', lastName: 'Joaquin', age: 23 },
-    { firstName: 'Barth', lastName: 'Kumar', age: 25 },
-    { firstName: 'John', lastName: 'Peter', age: 32 },
-    { firstName: 'Tazzo', lastName: 'Joaquin', age: 23 },
+    { id: 1, firstName: 'Prakash', lastName: 'Joaquin', age: 23 },
+    { id: 2, firstName: 'Barth', lastName: 'Kumar', age: 25 },
+    { id: 3, firstName: 'John', lastName: 'Peter', age: 32 },
+    { id: 4, firstName: 'Tazzo', lastName: 'Joaquin', age: 23 },
   ];
 
   const renderList = user
-    .filter((x) => x.age < 25)
+    .filter((x) => x.age < 25) //Filter BY Age!
+    .sort((a, b) => b.id - a.id) // sort in Accending Order!
     .map((x) => {
       return (
         <tr>
           <td>{x.firstName}</td>
         </tr>
-      );
+      ); //Map through the array
     });
   return (
     <div>
